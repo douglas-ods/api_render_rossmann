76ea364ea4d83a22c2e9e661ff41402ac8723b44
douglas-ods <douglas_ods@yahoo.com.br>
2023-02-24 19:16:05 -0300

    alter fyle rossmann

--- handler.py
+++ handler.py
@@ -1,5 +1,4 @@
 import pickle
-import os
 import pandas as pd
 from flask             import Flask, request, Response
 from rossmann.Rossmann import Rossmann
@@ -29,6 +28,5 @@ def rossmann_predict():
         return df_response        
     else:
         return Response("{}",status=200,mimetype="application/json")
-if __name__=="__main__": 
-     port= os.environ.get('PORT',5000)
-     app.run(host="0.0.0.0",port=port)
+if __name__=="__main__":      
+     app.run()
